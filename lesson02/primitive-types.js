39a101b9734b4209006fe75733dfc872e9d02a8e
Luís Falcão <lfalcao@cc.isel.ipl.pt>
2021-10-11 19:32:03 +0100

    Lesson02:  JavaScript basics types.

---
+++ lesson02/primitive-types.js
@@ -0,0 +1,24 @@
+let number = 12.3;
+console.log(typeof number)
+
+let string = 'SLB'
+console.log(typeof string)
+
+let boolean = true;
+console.log(typeof boolean)
+
+let undef = undefined;
+console.log(typeof undef)
+
+let nil = null;
+console.log(typeof nil)
+
+let f = function () { }
+console.log(typeof f)
+
+let array = [1,2,3];
+console.log("typeof " + array + " is " + typeof array)
+
+let object = {};
+console.log("typeof " + object + " is " + typeof object)
+
