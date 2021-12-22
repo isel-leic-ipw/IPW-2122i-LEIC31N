1adf6b94f826935ebd453d7e3039b5c109ca9391
Luís Falcão <lfalcao@cc.isel.ipl.pt>
2021-12-22 17:47:32 +0000

    Lesson 19: Copy from lesson 18

---
+++ lesson19/errors.js
@@ -0,0 +1,4 @@
+module.exports = {
+    INVALID_ID: {code: 1000, text: 'Invalid Id'},
+    NOT_FOUND: {code: 1001, text: 'Not found'}
+}
---
+++ lesson19/jokes-api.js
@@ -0,0 +1,66 @@
+// File rsponsibilities
+// Have the functions that handle HTTP requests and 
+// delegate all domain logic to jokes-services module
+
+const httpErrors = require('./http-errors')
+
+module.exports = function(jokesServices) {
+    if(!jokesServices) 
+        throw "Invalid argument for jokesData"
+
+    
+    return {
+        getJokes: getJokes,
+        getJoke: getJoke,
+        createJoke: createJoke,
+        deleteJoke: deleteJoke,
+        updateJoke: updateJoke
+    }
+    async function getJokes(req, rsp){
+        // jokesServices.getJokes()
+        //     .then(jokes => rsp.json(jokes))
+        //     .catch( e => rsp.status(500).json({description: "Internal error occurred"}))
+    
+        try {
+            const skip = req.query.skip ? undefined : Number(req.query.skip)
+            const limit = req.query.limit ? undefined : Number(req.query.limit)
+            //let userId = req.get('Authorization').split(' ')[1]
+            let userId = '0b115b6e-8fcd-4b66-ac26-33392dcb9340'
+            let jokes = await jokesServices.getJokes(userId, req.query.searchString, skip , limit)
+            rsp.json(jokes)
+        } catch(e) {
+            rsp.status(500).json({description: "Internal error occurred"})
+        }
+    }
+    
+    function getJoke(req, rsp){
+        jokesServices.getJoke(req.params.id)
+            .then(joke => rsp.json(joke))
+            .catch(e => processError(e, rsp))
+    }
+    
+    function processError(aplErr, rsp) {
+        const httpErr = httpErrors(aplErr)
+        console.log(httpErr)
+        rsp.status(httpErr.status).json(httpErr.body)
+    
+    
+    }
+    
+    function createJoke(req, resp){
+        jokesServices.createJoke(req.body.text)
+            .then((newJoke)=>resp.status(201).json(newJoke))
+            .catch((e)=>resp.status(500).json({message : `Server Error: ${e}`}))
+    }
+    
+    function updateJoke(req, rsp){
+        rsp.json({message : "updateJoke id = " + req.params.id })
+    }
+    
+    
+    function deleteJoke(req, rsp){ 
+        rsp.json({message : "deleteJoke " })
+    }    
+}
+
+
